c3ea40aefbd81ea87854382fc3bb46a2faa689a5
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2024-10-14 17:47:58 -0300

    Programação básica em Python

---
+++ python-basico-Kenneth_Lambert/aula013_name_main.py
@@ -0,0 +1,7 @@
+# if __name__ == "__main__":
+
+def mensagem():
+    print("Seja bem-vindo(a) ...")
+
+if __name__ == "__main__":
+    mensagem()
---
+++ python-basico-Kenneth_Lambert/aula014_while_for.py
@@ -0,0 +1,13 @@
+# while
+produto = 1
+contador = 1
+while contador <= 10:
+    produto = produto * contador
+    contador = contador + 1
+print(produto)
+
+# for
+produto = 1
+for contador in range(1,11,1):
+    produto = produto * contador
+print(produto)
