e51ee9ea70648e293ba6283ce98c8315cd9603ee
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2025-02-01 22:25:50 -0300

    aula018_chamadas_metodos

---
+++ python-basico-Kenneth_Lambert/aula018_chamadas_metodo.py
@@ -0,0 +1,15 @@
+# Objetos e chamadas de metodo
+
+# Metodos em strings
+string = "greater"
+
+print(string)
+print(string.isupper())
+print(string.upper())
+print(string.startswith("great"))
+
+# Metodos reconhecidos por um objeto
+# Sintaxe: dir(objeto)
+print(dir(string))
+# Documentacao sobre o uso de um metodo
+help(string.upper)
